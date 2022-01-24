ef592036fe17e26c3dcc545969477ea2b7e0e66a
Swetha Sri Vithala <swethavithala@gmail.com>
2022-01-24 23:42:11 +0530

    Add files via upload

---
+++ JS/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+	{
+		numb: 1,
+		question: "Question1",
+		answer: "Correct Answer",
+		options: [
+			"Correct Answer",
+			"Wrong Answer",
+			"Wrong Answer",
+			"Wrong Answer"
+		]
+	},
+	{
+		numb: 2,
+		question: "Question2",
+		answer: "Correct Answer",
+		options: [
+			"Correct Answer",
+			"Wrong Answer",
+			"Wrong Answer",
+			"Wrong Answer"
+		]
+	},
+	{
+		numb: 3,
+		question: "Question3",
+		answer: "Correct Answer",
+		options: [
+			"Correct Answer",
+			"Wrong Answer",
+			"Wrong Answer",
+			"Wrong Answer"
+		]
+	},
+	{
+		numb: 4,
+		question: "Question4",
+		answer: "Correct Answer",
+		options: [
+			"Correct Answer",
+			"Wrong Answer",
+			"Wrong Answer",
+			"Wrong Answer"
+		]
+	},
+	{
+		numb: 5,
+		question: "Question5",
+		answer: "Correct Answer",
+		options: [
+			"Correct Answer",
+			"Wrong Answer",
+			"Wrong Answer",
+			"Wrong Answer"
+		]
+	},
+];
